1c463eab927e3ab31732c0268655f707b8b505a4
enaki <enachi98@gmail.com>
2020-05-13 23:19:07 +0300

    Finished Lab 12

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
-
+var mongo = require('mongodb');
+var MongoClient = mongo.MongoClient;
+var ObjectId = mongo.ObjectID;
 const app = express();
 app.use(cookieParser());
 
@@ -30,19 +32,33 @@ app.use(session({
 }));
 session.user = null;
 
-// la accesarea din browser adresei http://localhost:6789/ se va returna textul 'Hello World'
 // proprietățile obiectului Request - req - https://expressjs.com/en/api.html#req
 // proprietățile obiectului Response - res - https://expressjs.com/en/api.html#res
-app.get('/', (req, res) =>{
+app.get('/', async (req, res) =>{
     console.log('cookies: ', req.cookies);
-    let user = req.cookies["utilizator"];
+    //let user = req.cookies["utilizator"];
     let username=null;
-    if (user){
-        username = user["nume"];
+    if (session.user){
+        //username = user["nume"];
+        username = session.user;
     }
     //console.log("User : ", user);
     res.clearCookie('utilizator');
-    res.render('index', {user: username});
+    let produse = await new Promise(((resolve, reject) => {
+        let url = "mongodb://localhost:27017/";
+        MongoClient.connect(url, {useUnifiedTopology: true,}, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("cumparaturi");
+            dbo.collection("produse").find({}).toArray(function(err, result) {
+                if (err) throw err;
+                resolve(result);
+                db.close();
+            });
+        });
+    }));
+    console.log(produse);
+    //res.render('index', {user: username, produse: produse});
+    res.render('index', {user: "fred", produse: produse});
 });
 
 const fs = require('fs');
@@ -50,6 +66,8 @@ let rawdata = fs.readFileSync('intrebari.json');
 let listaIntrebari = JSON.parse(rawdata);
 rawdata = fs.readFileSync('utilizatori.json');
 let users = JSON.parse(rawdata);
+rawdata = fs.readFileSync('movies.json');
+let produse = JSON.parse(rawdata);
 
 // la accesarea din browser adresei http://localhost:6789/chestionar se va apela funcția specificată
 app.get('/chestionar', (req, res) => {
@@ -60,7 +78,6 @@ app.get('/chestionar', (req, res) => {
 
 app.post('/rezultat-chestionar', (req, res) => {
     console.log(JSON.stringify(req.body));
-    //res.send("formular: " + JSON.stringify(req.body));
     res.render('rezultat-chestionar', {intrebari: listaIntrebari, raspunsuri: JSON.stringify(req.body)});
 });
 
@@ -74,6 +91,104 @@ app.get('/autentificare', (req, res) => {
     res.render('autentificare', {user: username});
 });
 
+function createDatabase() {
+    let url = "mongodb://localhost:27017/cumparaturi";
+    MongoClient.connect(url, {useUnifiedTopology: true,}, async function (err, db) {
+        if (err) throw err;
+        console.log("Database created!");
+        let dbo = db.db("cumparaturi");
+        await dbo.collection("produse").drop( function(err, delOK) {
+            if (err) throw err;
+            if (delOK) console.log("Collection deleted");
+            dbo.createCollection("produse", function(err, res) {
+                if (err) throw err;
+                console.log("Collection created!");
+                db.close();
+            })
+        });
+    });
+}
+
+
+function insertProduse(){
+    let url = "mongodb://localhost:27017/";
+
+    MongoClient.connect(url, {useUnifiedTopology: true,}, function(err, db) {
+        if (err) throw err;
+        let dbo = db.db("cumparaturi");
+        console.log(produse);
+        let myobj = {name: "Company Inc", address: "Highway 37"};
+        dbo.collection("produse").insertMany(produse, function(err, res) {
+            if (err) throw err;
+            console.log("1 document inserted");
+            db.close();
+        });
+    });
+}
+
+app.get("/creare-bd", async (req, res) => {
+    await createDatabase();
+    res.redirect("/");
+});
+
+app.get("/inserare-bd", async (req, res) => {
+    await insertProduse();
+    res.redirect("/");
+});
+
+function getProduct(id){
+    return new Promise(((resolve, reject) => {
+        let url = "mongodb://localhost:27017/";
+        MongoClient.connect(url, {useUnifiedTopology: true,}, function(err, db) {
+            if (err) throw err;
+            let dbo = db.db("cumparaturi");
+            let query = {"_id": ObjectId(id)};
+            console.log(id);
+            dbo.collection("produse").find(query).toArray(function(err, result) {
+                if (err) throw err;
+                if (result.length > 0){
+                    resolve(result[0]);
+                } else {
+                    resolve(result);
+                }
+                db.close();
+            });
+        });
+    }));
+}
+
+app.get("/vizualizare-cos", async (req, res) => {
+    console.log("------------Vizualizare-----------")
+    let detailed_items = []
+    if (req.session.cart){
+        let array_elements = req.session.cart;
+        console.log(array_elements);
+        array_elements.sort();
+        let current = null;
+        let cnt = 0;
+        for (let i = 0; i < array_elements.length; i++) {
+            if (array_elements[i] != current) {
+                if (cnt > 0) {
+                    let produs = await getProduct(current);
+                    console.log(produs)
+                    detailed_items.push({titlu: produs.titlu, price: produs.price, cantitate: cnt});
+                }
+                current = array_elements[i];
+                cnt = 1;
+            } else {
+                cnt++;
+            }
+        }
+        if (cnt > 0) {
+            let produs = await getProduct(current);
+            console.log(produs)
+            detailed_items.push({titlu: produs.titlu, price: produs.price, cantitate: cnt});
+        }
+        //console.log(detailed_items);
+    }
+    //res.render('index', {user: username, produse: produse});
+    res.render("vizualizare-cos", {user: "fred", produse : detailed_items});
+});
 
 app.post('/verificare-autentificare', (req, res) => {
     let user = req.body;
@@ -89,6 +204,17 @@ app.post('/verificare-autentificare', (req, res) => {
     }
 
     res.redirect('http://localhost:6789');
+});
+
+app.post('/adaugare_cos', (req, res) => {
+    let item = req.body.id;
+    console.log(item);
+    if (req.session.cart === undefined){
+        req.session.cart = [];
+    }
+    req.session.cart.push(item);
+    //res.sendStatus(200);
+    res.redirect("/");
 });
 
 app.get('/log-out', (req, res) => {
